05151a061a4dcd3f50007ad9644d72aadca795c8
NickSigurdsson <nicholassigurdsson@gmail.com>
2022-12-09 17:52:14 -0500

    homepage news list component

--- src/components/NewsList/NewsList.js
+++ src/components/NewsList/NewsList.js
@@ -1,11 +1,20 @@
 import './NewsList.scss';
 import NewsItem from '../NewsItem/NewsItem';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 function NewsList(){
+    useEffect(()=>{
+        axios.get('http://localhost:8080/')
+        .then(response=>{
+            console.log(response.data);
+        })
+        .catch(error=>{
+            console.log('something went wrong');
+        })
+        
+    },[])
     return(
-        // useEffect
         <NewsItem/>
-
     )
 }
 export default NewsList;
