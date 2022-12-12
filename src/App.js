c14a4d3510d24edbf520c16c0ce8dc90b44aef3c
NickSigurdsson <nicholassigurdsson@gmail.com>
2022-12-12 13:57:36 -0500

    finished logic, need testing when API call limit is refreshed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.scss';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Header from './components/Header/Header';
 import Homepage from './pages/Homepage';
-import Calendar from './pages/Calendar';
+import CalendarPage from './pages/Calendar';
 import Dashboard from './pages/Dashboard';
 
 function App() {
@@ -13,9 +13,9 @@ function App() {
         {/* Homepage */}
         <Route path='/' element={<Homepage/>}></Route>
         {/* Calendar */}
-        <Route path='/' element={<Calendar/>}></Route>
+        <Route path='/calendar' element={<CalendarPage/>}></Route>
         {/* Dashboard */}
-        <Route path='/' element={<Dashboard/>}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,6 +1,63 @@
-function Calendar(){
+import {useState, useEffect} from 'react';
+import CalendarComponent from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import axios from 'axios';
+function CalendarPage(){ 
+    //  new date = date object with the current date and time (makes sure that the state has the current date value)
+    const [value, setValue] = useState(new Date());
+    // console.log(value);
+    const [dateInfo, setDateInfo] = useState([]);
+
+    // this function will run when the calendar loads AND when the date selected is changed
+    useEffect(() => {
+        axios.get('http://localhost:8080/calendar')
+        .then(response => {
+            setDateInfo(response.data)
+            console.log(dateInfo);
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }, []);
+    
+    if (!dateInfo) {
+        return (
+            <h1>Currently fetching list of warehouses</h1>
+        )
+    }
+
+    // function currentDateInfo(value){
+    // // if value of the date is equal to the api's mapped value then display the type key value pair from the api
+    //     dateInfo.map((dateData)=>{
+    //         if (dateData.date === value){
+    //             const importantDates = dateData.type;
+    //             return importantDates;
+    //         }
+    //         const importantDates = 'No important events today';
+    //         return importantDates;
+    //     })
+    // }
     return(
-        <div>hello</div>
-    )
+        <>
+            <section>
+                {/* the moment the value is changed, we will run the on change function (which in this case is the "set value of react" 
+                and we will pass in the new input value into the value to change it accordingly*/}
+                <CalendarComponent onChange={setValue} value={value}/>
+            </section>
+            <section className='current-date-info'>
+                <h2>Important events</h2>
+                <p>
+                    {dateInfo.filter((dateData)=>{
+                        if (dateData.date === value){
+                            const importantDates = dateData.type;
+                            return importantDates;
+                        }
+                        const importantDates = 'No important events today';
+                        return importantDates;
+                    })}
+                </p>
+            </section>
+        </>
+    );
 }
-export default Calendar;
+export default CalendarPage;
