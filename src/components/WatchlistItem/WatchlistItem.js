53c7fd2077e27e6ae8b8899c79fca9457fbf7a05
NickSigurdsson <nicholassigurdsson@gmail.com>
2023-01-09 03:37:18 -0500

    watchlist ticker % change logic done

--- src/components/WatchlistItem/WatchlistItem.js
+++ src/components/WatchlistItem/WatchlistItem.js
@@ -1,10 +1,24 @@
 import './WatchlistItem.scss'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function WatchlistItem({ticker,price}){
+    const [priceQuote, setPriceQuote] = useState('')
+    useEffect(()=>{
+        axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=3NSM9679F4Z9LTNT`)
+        .then(response=>{
+            setPriceQuote(response.data["Global Quote"]["10. change percent"]);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+    },[ticker])
+
     return(
         <ul className="watchlist-item">
             <li className="watchlist-item__data">{ticker}</li>
             <li className="watchlist-item__data">{price}</li>
-            <li className="watchlist-item__data-price">5.1%</li>
+            <li className="watchlist-item__data-price">{priceQuote}</li>
         </ul>
     )
 }
